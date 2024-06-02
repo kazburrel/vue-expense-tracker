0b5df0c7ec4a580d24c14fafc3c9162814fa232f
kazburrel <oegbosionu@gmail.com>
2024-06-02 22:14:40 +0100

    set up

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  My App
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+  </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+  },
+};
+</script>
---
+++ src/components/Balance.vue
@@ -0,0 +1,4 @@
+<template>
+    <h4>You Balance</h4>
+    <h1 class="balance">$0.00</h1>
+</template>
