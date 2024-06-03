39b02f3c69422929a556690ea6d10c7222cd6d65
kazburrel <oegbosionu@gmail.com>
2024-06-03 08:51:33 +0100

    toaster setup and addtransaction setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
+    <Balance :total="+total" />
     <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -14,9 +14,10 @@ import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
+import { useToast } from 'vue-toastification';
 
 import { ref, computed } from 'vue'
-
+const toast = useToast();
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -19.99 },
   { id: 2, text: 'Salary', amount: 799.97 },
@@ -49,4 +50,20 @@ const expenses = computed(() => {
     }, 0)
     .toFixed(2);
 })
+
+// Handle transaction 
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  })
+  toast.success('Transaction added');
+
+}
+
+// GEnerate unique ID
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+}
 </script>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,19 +3,36 @@
     <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text....">
+            <input type="text" v-model="text" id="text" placeholder="Enter text....">
         </div>
         <div class="form-control">
 
             <label for="amount">Amount <br> (negative - expense, positive - income)</label>
-            <input type="number" name="" id="number" placeholder="Enter amount...">
+            <input type="text" v-model="amount" name="" id="number" placeholder="Enter amount...">
         </div>
         <button class="btn">Add transaction</button>
     </form>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useToast } from 'vue-toastification';
+const text = ref('');
+const amount = ref('');
+const emit = defineEmits('transactionSubmitted');
+const toast = useToast();
 const onSubmit = () => {
-    console.log('submit');
+    if (!text.value || !amount.value) {
+        toast.error('Both fields are required');
+        return;
+    }
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value),
+    }
+    emit('transactionSubmitted', transactionData)
+    text.value = '';
+    amount.value = ''
+
 }
 </script>
